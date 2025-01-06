2429dcf0bd626d1b4615a63f33145b961b4ea56a
Ebrahim Morkas <morkasebrahim3@gmail.com>
2025-01-06 11:48:37 +0530

    Model created for expenses card thatr is blueprint

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:expense_tracker/expenses_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: ExpensesScreen(),
+    ),
+  ));
+}
