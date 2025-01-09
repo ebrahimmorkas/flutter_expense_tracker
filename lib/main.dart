46ad74123573be087e77b4ad697a3deb6db2c618
Ebrahim Morkas <morkasebrahim3@gmail.com>
2025-01-09 17:45:56 +0530

    Dark mode of the app is created which can be toggled based on the system default theme mode

--- lib/main.dart
+++ lib/main.dart
@@ -5,34 +5,44 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: ExpensesScreen(),
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+      ),
       theme: ThemeData().copyWith(
-          colorScheme: kColorScheme,
-          appBarTheme: AppBarTheme().copyWith(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.primaryContainer,
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            )),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
           ),
-          cardTheme: CardTheme().copyWith(
-              color: kColorScheme.secondaryContainer,
-              margin: EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 8,
-              )),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: kColorScheme.primaryContainer,
-            ),
-          ),
-          textTheme: ThemeData().textTheme.copyWith(
-                  titleLarge: TextStyle(
-                fontWeight: FontWeight.normal,
-                color: kColorScheme.onSecondaryContainer,
-                fontSize: 14,
-              ))),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: kColorScheme.onSecondaryContainer,
+              fontSize: 14,
+            )),
+      ),
+      themeMode: ThemeMode
+          .system, // If the users phone is in dark mode the app will run in dark mode and vice versa
     ),
   );
 }
